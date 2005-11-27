1dfb633047a3d1ebfb9d78dc5ecff1fe81899af7
halt1983 <halt1983@2ef88817-412d-0410-a32c-8029a115e976>
2005-11-27 16:25:04 +0000

    - Fix Bug#6566

--- skel/skel.action.php
+++ skel/skel.action.php
@@ -30,7 +30,7 @@ class {$action_form} extends Ethna_ActionForm
 			'regexp'        => null,			// 文字種指定(正規表現)
 			'custom'        => null,			// メソッドによるチェック
 			'filter'        => null,			// 入力値変換フィルタオプション
-			'form_type'		=> FORM_TYPE_TEXT	// フォーム型
+			'form_type'     => FORM_TYPE_TEXT,	// フォーム型
 			'type'          => VAR_TYPE_INT,	// 入力値型
 		),
 		*/
