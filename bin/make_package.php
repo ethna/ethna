0d3d44bb86338de24d8c28f2dc424f6d4ccc9e49
halt1983 <halt1983@2ef88817-412d-0410-a32c-8029a115e976>
2006-01-16 15:45:09 +0000

    - support channel server

--- bin/make_package.php
+++ bin/make_package.php
@@ -4,32 +4,52 @@
  *
  * @category pear
  */
-require_once('PEAR/PackageFileManager.php');
+ 
+// http://pear.php.net/package/PEAR_PackageFileManager/docs/1.6.0a4/PEAR_PackageFileManager/PEAR_PackageFileManager2.html#methodaddReplacement
+require_once('PEAR/PackageFileManager2.php');
+
+$description = 'Ethna is PHP WebApplication Framework Package.';
+$varsion = '0.2.0';
+
 $config = array(
-    'package' => 'Ethna',
     'baseinstalldir' => 'Ethna',
-    'version' => '0.2.0',
     'packagedirectory' => dirname(dirname(__FILE__)),
-    'summary' => 'Ethna PHP Framework Packages',
-    'notes'    => 'Ethna PHP',
-    'state' => 'alpha',
-    'license' => 'BSD',
     'filelistgenerator' => 'file',
     'ignore' => array('CVS/', 'make_package.php', 'package.xml'),
     'changelogoldtonew' => false,
-    'description' => 'Ethna Framework Package',
+    'description' => $description,
     'simpleoutput' => true,
+
     );
  
 print("Start Script\n");
-
-$packagexml = new PEAR_PackageFileManager;
+$packagexml = new PEAR_PackageFileManager2;
 $packagexml->setOptions($config);
+$packagexml->setPackage('Ethna');
+$packagexml->setSummary('Ethna PHP Framework Package');
+$packagexml->setDescription($description);
+$packagexml->setLicense('BSD', 'http://sourceforge.jp/projects/ethna/');
+$packagexml->setReleaseVersion($varsion);
+$packagexml->setAPIVersion($varsion);
+$packagexml->setReleaseStability('alpha');
+$packagexml->setAPIStability('alpha');
+$packagexml->setNotes('Ethna Dev');
+
+$packagexml->setPackageType('php');
 $packagexml->addRole('tpl', 'php');
-$packagexml->addMaintainer('cocoitiban', 'contributor', 'Keita Arai', 'cocoiti@comio.info');
-$packagexml->addMaintainer('halt', 'lead', 'halt feits', 'halt.hde@gmail.com');
-$packagexml->addDependency('PEAR', '1.3.5', 'ge', 'pkg', false);
-$packagexml->addDependency('php', '4.1.0', 'ge', 'php', false);
+
+$packagexml->addMaintainer('lead', 'halt' , 'halt feits', 'halt.hde@gmail.com');
+$packagexml->addMaintainer('contributor', 'cocoitiban', 'Keita Arai', 'cocoiti@comio.info');
+
+$packagexml->setPhpDep('4.1.0');
+$packagexml->setPearinstallerDep('1.3.5');
+$packagexml->setPackageType('php');
+
+//$packagexml->setChannel('channel.php.gr.jp');
+$packagexml->setChannel('pear.php.net');
+
+$packagexml->generateContents();
+
 
 // note use of debugPackageFile() - this is VERY important
 if (isset($_GET['make']) || $_SERVER['argv'][1] == 'make') {
