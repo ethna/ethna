fce5d5c0c9d6de73f3311ab10faa4e7f38bdba63
fujimoto <fujimoto@2ef88817-412d-0410-a32c-8029a115e976>
2006-05-16 08:47:06 +0000

    - ethnaコマンドに-v(--version)オプションを追加

--- bin/ethna_handle.php
+++ bin/ethna_handle.php
@@ -36,6 +36,21 @@ $getopt =& new Console_Getopt();
 $arg_list = $getopt->readPHPArgv();
 array_shift($arg_list);
 
+list($my_arg_list, $arg_list) = _Ethna_HandleGateway_SeparateArgList($arg_list);
+$r = $getopt->getopt($my_arg_list, "v", array("version"));
+if (Ethna::isError($r)) {
+    usage($ehm);
+    exit(1);
+}
+
+// ad-hoc:(
+foreach ($r[0] as $opt) {
+    if ($opt[0] == "v" || $opt[0] == "--version") {
+        _Ethna_HandleGateway_ShowVersion();
+        exit(2);
+    }
+}
+
 $ehm =& new Ethna_Handle_Manager();
 if (count($arg_list) == 0) {
     usage($ehm);
@@ -68,10 +83,52 @@ if (Ethna::isError($r)) {
 function usage(&$ehm)
 {
     $handler_list = $ehm->getHandlerList();
-    printf("usage: ethna [command] [args...]\n\n");
-    printf("available commands are as follows:\n\n");
+    printf("usage: ethna [option] [command] [args...]\n\n");
+    printf("available options are as follows:\n\n");
+    printf("  -v, --version    show version and exit\n");
+    printf("\navailable commands are as follows:\n\n");
     foreach ($handler_list as $handler) {
         printf("  %s -> %s\n", $handler->getId(), $handler->getDescription());
     }
+}
+
+/**
+ *  fetch options for myself
+ */
+function _Ethna_HandleGateway_SeparateArgList($arg_list)
+{
+    $my_arg_list = array();
+    for ($i = 0; $i < count($arg_list); $i++) {
+        if ($arg_list[$i]{0} == '-') {
+            // assume this should be an option for myself
+            $my_arg_list[] = $arg_list[$i];
+        } else {
+            break;
+        }
+    }
+    $arg_list = array_slice($arg_list, $i);
+
+    return array($my_arg_list, $arg_list);
+}
+
+/**
+ *  show version
+ */
+function _Ethna_HandleGateway_ShowVersion()
+{
+    $version = <<<EOD
+Ethna %s
+
+Copyright (c) 2004-2006,
+  Masaki Fujimoto <fujimoto@php.net>
+  halt feits <halt.feits@gmail.com>
+  Takuya Ookubo <sfio@sakura.ai.to>
+  nozzzzz <nozzzzz@gmail.com>
+  cocoitiban <cocoiti@comio.info>
+
+http://ethna.jp/
+
+EOD;
+    printf($version, ETHNA_VERSION);
 }
 ?>
