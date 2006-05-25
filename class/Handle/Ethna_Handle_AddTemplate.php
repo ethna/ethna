258a078ef0f4c8fbc1c1c084215d205f839ed10a
halt1983 <halt1983@2ef88817-412d-0410-a32c-8029a115e976>
2006-05-25 03:11:55 +0000

    - add-templateコマンド追加(by nnno)

---
+++ class/Handle/Ethna_Handle_AddTemplate.php
@@ -0,0 +1,92 @@
+<?php
+// vim: foldmethod=marker
+/**
+ *	Ethna_Handle_AddTemplate.php
+ *
+ *	@author	    nnno <nnno@nnno.jp>	
+ *	@license	http://www.opensource.org/licenses/bsd-license.php The BSD License
+ *	@package	Ethna
+ */
+
+// {{{ Ethna_Handle_AddTemplate
+/**
+ *  add-template handler
+ *
+ *	@author     nnno <nnno@nnno.jp>
+ *	@access		public
+ *	@package	Ethna
+ */
+class Ethna_Handle_AddTemplate extends Ethna_Handle
+{
+    /**
+     *  get handler's description
+     *
+     *  @access public
+     */
+    function getDescription()
+    {
+        return "add new template to project:\n    {$this->id} [template] ([project-base-dir])\n";
+    }
+
+    /**
+     *  add template 
+     *
+     *  @access public
+     */
+    function perform()
+    {
+        $r = $this->_validateArgList();
+        if (Ethna::isError($r)) {
+            return $r;
+        }
+        list($template, $app_dir) = $r;
+
+        $sg =& new Ethna_SkeltonGenerator();
+        $r = $sg->generateTemplateSkelton($template, $app_dir);
+        if (Ethna::isError($r)) {
+            printf("error occurred while generating skelton. please see also following error message(s)\n\n");
+            return $r;
+        }
+
+        return true;
+    }
+
+    /**
+     *  show usage
+     *
+     *  @access public
+     */
+    function usage()
+    {
+        printf("usage:\nethna %s [template] ([project-base-dir])\n", $this->id);
+    }
+
+    /**
+     *  check arguments
+     *
+     *  @access private
+     */
+    function _validateArgList()
+    {
+        $arg_list = array();
+        if (count($this->arg_list) < 1) {
+            return Ethna::raiseError('too few argments', 'usage');
+        } else if (count($this->arg_list) > 2) {
+            return Ethna::raiseError('too many argments', 'usage');
+        } else if (count($this->arg_list) == 1) {
+            $arg_list[] = $this->arg_list[0];
+            $arg_list[] = getcwd();
+        } else {
+            $arg_list = $this->arg_list;
+        }
+
+        // TODO: check action name(?) - how it would be easy and pluggable
+        if (is_dir($arg_list[1]) == false) {
+            return Ethna::raiseError("no such directory [{$arg_list[1]}]");
+        }
+
+        return $arg_list;
+    }
+}
+// }}}
+?>
