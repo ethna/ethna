47443a3d8db61fb4f30dfa70952bbe4a57119b6d
fujimoto <fujimoto@2ef88817-412d-0410-a32c-8029a115e976>
2006-06-05 14:16:47 +0000

    - iniファイルのスケルトンにdsnサンプル追加

--- skel/etc.ini.php
+++ skel/etc.ini.php
@@ -5,13 +5,31 @@
  * update:
  */
 $config = array(
-	'debug'	=> false,
-	'log_facility'  => 'echo',
-	'log_level' => 'warning',
-	'log_option' => 'pid,function,pos',
-	'log_alert_level' => 'crit',
-	'log_alert_mailaddress' => '',
-	'log_filter_do' => '',
-	'log_filter_ignore' => 'Undefined index.*%%.*tpl',
+    // debug
+    'debug'	=> false,
+
+    // db
+    // sample-1: single db
+    // 'dsn' => 'mysql://user:password@server/database',
+    //
+    // sample-2: single db w/ multiple users
+    // 'dsn'   => 'mysql://rw_user:password@server/database', // read-write
+    // 'dsn_r' => 'mysql://ro_user:password@server/database', // read-only
+    //
+    // sample-3: multiple db (slaves)
+    // 'dsn'   => 'mysql://rw_user:password@master/database', // read-write(master)
+    // 'dsn_r' => array(
+    //     'mysql://ro_user:password@slave1/database',         // read-only(slave)
+    //     'mysql://ro_user:password@slave2/database',         // read-only(slave)
+    // ),
+
+    // log
+    'log_facility'  => 'echo',
+    'log_level' => 'warning',
+    'log_option' => 'pid,function,pos',
+    'log_alert_level' => 'crit',
+    'log_alert_mailaddress' => '',
+    'log_filter_do' => '',
+    'log_filter_ignore' => 'Undefined index.*%%.*tpl',
 );
 ?>
