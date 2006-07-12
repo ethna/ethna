808edee0c4ea6bd52ac38b1bcaa95581bb215471
halt1983 <halt1983@2ef88817-412d-0410-a32c-8029a115e976>
2006-07-12 10:39:11 +0000

    add Ethna_ViewClass_Test

---
+++ test/Ethna_ViewClass_Test.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ *  Ethna_ViewClass_Test.php
+ *
+ *  @author halt feits <halt.feits@gmail.com>
+ */
+
+//error_reporting(E_ALL);
+
+/**
+ *  Ethna_ViewClassクラスのテストケース
+ *
+ *  @access public
+ */
+class Ethna_ViewClass_Test extends UnitTestCase
+{
+    /**
+     * Ethna_Controller
+     *
+     * @var     Ethna_Controller
+     * @access  protected
+     */
+    var $ctl;
+
+    /**
+     * Ethna_Backend
+     *
+     * @var     Ethna_Backend
+     * @access  protected
+     */
+    var $backend;
+
+    /**
+     * Ethna_ViewClass
+     * @var     Ethna_ViewClass
+     * @access  protected
+     */
+    var $viewclass;
+
+    /**
+     * setUp
+     *
+     * make Ethna_ViewClass class
+     *
+     * @access public
+     * @param void
+     */
+    function setUp()
+    {
+        $this->ctl =& new Ethna_Controller();
+        $this->ctl->action_form =& new Ethna_ActionForm($this->ctl);
+
+        $this->backend =& $this->ctl->getBackend();
+        $this->viewclass =& new Ethna_ViewClass($this->backend, '', '');
+    }
+
+    /**
+     * test_getFormInput_Html
+     *
+     * @access public
+     * @param void
+     */
+    function test_getFormInput_Html()
+    {
+        $actual = '<input type="text" name="test" value="1" />';
+
+        $test_attr = array(
+            'type' => 'text',
+            'name' => 'test',
+            'value' => '1',
+        );
+
+        $user_attr = array();
+
+        $result = $this->viewclass->_getFormInput_Html('input', $test_attr, $user_attr);
+
+        $this->assertEqual($result, $actual);
+    }
+
+    function test_getFormInput_Text()
+    {
+        $name = "test_text";
+        $def = array(
+            'max' => 20,
+        );
+        $params = array();
+
+        $test_form = array(
+            'test_text' => array(
+                'name' => 'TestTestText',
+                'form_type' => FORM_TYPE_TEXT,
+                'type' => VAR_TYPE_STRING,
+            ),                    
+        );
+
+        $this->viewclass->af->form = $test_form;
+
+        $result = $this->viewclass->_getFormInput_Text($name, $def, $params);
+        //var_dump($result);
+    }
+
+    function testGetFormName()
+    {
+        $test_word = "TestTestTest";
+
+        $test_form = array(
+            'test_text' => array(
+                'name' => $test_word,
+                'form_type' => FORM_TYPE_TEXT,
+                'type' => VAR_TYPE_STRING,
+            ),                    
+        );
+        $params = array();
+
+        $this->viewclass->af->form = $test_form;
+
+        $result = $this->viewclass->getFormName('test_text', $params);
+        $this->assertEqual($result, $test_word);
+     }
+
+}
+?>
