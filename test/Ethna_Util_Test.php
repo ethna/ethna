336e0c1463865b64caa9486cb35efe3067142457
halt1983 <halt1983@2ef88817-412d-0410-a32c-8029a115e976>
2006-09-02 14:59:23 +0000

    add Ethna_Util test

--- test/Ethna_Util_Test.php
+++ test/Ethna_Util_Test.php
@@ -12,11 +12,47 @@ class Ethna_Util_Test extends UnitTestCase
 {
     function testCheckMailAddress()
     {
+        $fail_words = array(
+            'hogehuga.net',
+            'untarakantara',
+            'example@example',
+            'example@.com',
+            'example@example@example.com',
+        );
+
+        foreach ($fail_words as $word) {
+            $this->assertFalse(Ethna_Util::checkMailAddress($word));
+        }
+        
         $util = new Ethna_Util;
         $result = $util->checkMailAddress('hogefuga.net');
         $this->assertFalse($result);
+
         $result = $util->checkMailAddress('hoge@fuga.net');
         $this->assertTrue($result);
     }
+
+    function testIsAbsolute()
+    {
+        $absolute_paths = array(
+            '/root',
+            '/home/user/giza',
+        );
+
+        $invalid_params = array(
+            '',
+            false,
+            true,
+            '0x1',
+        );
+
+        foreach ($absolute_paths as $path) {
+            $this->assertTrue(Ethna_Util::isAbsolute($path));
+        }
+        
+        foreach ($invalid_params as $path) {
+            $this->assertFalse(Ethna_Util::isAbsolute($path));
+        }
+     }
 }
 ?>
