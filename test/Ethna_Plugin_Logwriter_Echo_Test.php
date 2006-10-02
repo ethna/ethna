9fcc4f80d5f9dd7e9a45960f7fa46bce3ecc0db8
halt1983 <halt1983@2ef88817-412d-0410-a32c-8029a115e976>
2006-10-02 08:04:55 +0000

    added test for Bug#9009

--- test/Ethna_Plugin_Logwriter_Echo_Test.php
+++ test/Ethna_Plugin_Logwriter_Echo_Test.php
@@ -10,39 +10,72 @@
  */
 class Ethna_Plugin_Logwriter_Echo_Test extends UnitTestCase
 {
+    var $ctl;
+    var $plugin;
+    var $lw;
+
+    function Ethna_Plugin_Logwriter_Echo_Test($label = false)
+    {
+        $this->ctl =& Ethna_Controller::getInstance();
+        $this->plugin =& $this->ctl->getPlugin();
+        $this->lw = $this->plugin->getPlugin('Logwriter', 'Echo');
+
+        $option = array(
+                        'ident' => 'testident',
+                        'facility' => 'mail',
+                        );
+        $this->lw->setOption($option);
+ 
+        parent::UnitTestCase($label);
+    }
+
     function testLogwriterEcho()
     {
-		$ctl =& Ethna_Controller::getInstance();
-		$plugin =& $ctl->getPlugin();
-		$lw = $plugin->getPlugin('Logwriter', 'Echo');
+        $message = 'comment';
 
-		$option = array(
-						'ident' => 'testident',
-						'facility' => 'mail',
-						);
-		$lw->setOption($option);
-		$message = 'comment';
+        $level_array = array(LOG_EMERG,
+            LOG_ALERT,
+            LOG_CRIT,
+            LOG_ERR,
+            LOG_WARNING,
+            LOG_NOTICE,
+            LOG_INFO,
+            LOG_DEBUG,);
 
-		$level_array = array(
-							 LOG_EMERG,
-							 LOG_ALERT,
-							 LOG_CRIT,
-							 LOG_ERR,
-							 LOG_WARNING,
-							 LOG_NOTICE,
-							 LOG_INFO,
-							 LOG_DEBUG,
-							 );
+        foreach ($level_array as $level) {
+            ob_start();         // コンソールへの出力をキャプチャ開始
 
-		foreach($level_array as $level){
-			ob_start();			// コンソールへの出力をキャプチャ開始
-			// 関数が返す文字列に改行タグ付与の是非
-			$funcout = $lw->log($level, $message)
-				. sprintf("%s", $ctl->getGateway() != GATEWAY_WWW ? "" : "<br />");
-			$stdout = trim(ob_get_contents());
-			$this->assertEqual($funcout, $stdout);
-			ob_end_clean();		// コンソールへの出力をキャプチャ終了
-		}
+            // 関数が返す文字列に改行タグ付与の是非
+            $funcout = $this->lw->log($level, $message)
+                . sprintf("%s", $this->ctl->getGateway() != GATEWAY_WWW ? "" : "<br />");
+
+            $stdout = trim(ob_get_contents());
+            $this->assertEqual($funcout, $stdout);
+
+            ob_end_clean();     // コンソールへの出力をキャプチャ終了
+        }
+    }
+
+    /**
+     * testBug9009
+     *
+     * @see http://sourceforge.jp/tracker/index.php?func=detail&aid=9009&group_id=1343&atid=5092
+     */
+    function testBug9009()
+    {
+        $level = LOG_WARNING;
+        $message = "SELECT * FROM item WHERE name LIKE '%salt%';";
+
+        ob_start();         // コンソールへの出力をキャプチャ開始
+
+        // 関数が返す文字列に改行タグ付与の是非
+        $funcout = $this->lw->log($level, $message)
+            . sprintf("%s", $this->ctl->getGateway() != GATEWAY_WWW ? "" : "<br />");
+
+        $stdout = trim(ob_get_contents());
+        $this->assertEqual($funcout, $stdout);
+
+        ob_end_clean();     // コンソールへの出力をキャプチャ終了
     }
 }
 ?>
