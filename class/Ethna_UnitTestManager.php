ec535991e9fb7c8518080b5e844abdc5654c138b
mumumu-org <mumumu-org@2ef88817-412d-0410-a32c-8029a115e976>
2007-07-12 11:27:25 +0000

    - fixed bug disabled view unit testing.(thx: sfio)

--- class/Ethna_UnitTestManager.php
+++ class/Ethna_UnitTestManager.php
@@ -8,9 +8,9 @@
  *  @version    $Id$
  */
 
-require_once 'simpletest/unit_tester.php';
-require_once 'Ethna_UnitTestCase.php';
-require_once 'Ethna_UnitTestReporter.php';
+require_once('simpletest/unit_tester.php');
+require_once('Ethna_UnitTestCase.php');
+require_once('Ethna_UnitTestReporter.php');
 
 /**
  *  Ethnaユニットテストマネージャクラス
@@ -41,6 +41,178 @@ class Ethna_UnitTestManager extends Ethna_AppManager
     }
 
     /**
+     *  定義済みアクション一覧を取得する
+     *
+     *  @access public
+     *  @return array   アクション一覧
+     */
+    function _getActionList()
+    {
+        $im =& new Ethna_InfoManager($this->backend);
+        return $im->getActionList();
+    }
+
+    /**
+     *  クラス名からビュー名を取得する
+     *
+     *  @access public
+     *  @param  string  $class_name     ビュークラス名
+     *  @return string  アクション名
+     */
+    function viewClassToName($class_name)
+    {
+        $prefix = sprintf("%s_View_", $this->ctl->getAppId());
+        if (preg_match("/$prefix(.*)/", $class_name, $match) == 0) {
+            // 不明なクラス名
+            return null;
+        }
+        $target = $match[1];
+
+        $action_name = substr(preg_replace('/([A-Z])/e', "'_' . strtolower('\$1')", $target), 1);
+
+        return $action_name;
+    }
+
+    /**
+     *  指定されたクラス名を継承しているかどうかを返す
+     *
+     *  @access private
+     *  @param  string  $class_name     チェック対象のクラス名
+     *  @param  string  $parent_name    親クラス名
+     *  @return bool    true:継承している false:いない
+     */
+    function _isSubclassOf($class_name, $parent_name)
+    {
+        while ($tmp = get_parent_class($class_name)) {
+            if (strcasecmp($tmp, $parent_name) == 0) {
+                return true;
+            }
+            $class_name = $tmp;
+        }
+        return false;
+    }
+
+    /**
+     *  ビュースクリプトを解析する
+     *
+     *  @access private
+     *  @param  string  $script ファイル名
+     *  @return array   ビュークラス定義一覧
+     */
+    function __analyzeViewScript($script)
+    {
+        $class_list = array();
+
+        $source = "";
+        $fp = fopen($script, 'r');
+        if ($fp == false) {
+            return null;
+        }
+        while (feof($fp) == false) {
+            $source .= fgets($fp, 8192);
+        }
+        fclose($fp);
+
+        // トークンに分割してクラス定義情報を取得
+        $token_list = token_get_all($source);
+        for ($i = 0; $i < count($token_list); $i++) {
+            $token = $token_list[$i];
+
+            if ($token[0] == T_CLASS) {
+                // クラス定義開始
+                $i += 2;
+                $class_name = $token_list[$i][1];       // should be T_STRING
+                if ($this->_isSubclassOf($class_name, 'Ethna_ViewClass')) {
+                    $view_name = $this->viewClassToName($class_name);
+                    $class_list[$view_name] = array(
+                        'template_file' => $this->ctl->_getForwardPath($view_name),
+                        'view_class' => $class_name,
+                        'view_class_file' => $this->ctl->getDefaultViewPath($view_name),
+                    );
+                }
+            }
+        }
+
+        if (count($class_list) == 0) {
+            return null;
+        }
+        return $class_list;
+    }
+
+    /**
+     *  ディレクトリ以下のビュースクリプトを解析する
+     *
+     *  @access private
+     *  @param  string  $action_dir     解析対象のディレクトリ
+     *  @return array   ビュークラス定義一覧
+     */
+    function __analyzeViewList($view_dir = null)
+    {
+        $r = array();
+
+        if (is_null($view_dir)) {
+            $view_dir = $this->ctl->getViewdir();
+        }
+        $prefix_len = strlen($this->ctl->getViewdir());
+
+        $ext = '.' . $this->ctl->getExt('php');
+        $ext_len = strlen($ext);
+
+        $dh = opendir($view_dir);
+        if ($dh) {
+            while (($file = readdir($dh)) !== false) {
+                $path = "$view_dir/$file";
+                if ($file != '.' && $file != '..' && is_dir($path)) {
+                    $tmp = $this->__analyzeViewList($path);
+                    $r = array_merge($r, $tmp);
+                    continue;
+                }
+                if (substr($file, -$ext_len, $ext_len) != $ext) {
+                    continue;
+                }
+
+                include_once($path);
+                $class_list = $this->__analyzeViewScript($path);
+                if (is_null($class_list) == false) {
+                    $r = array_merge($r, $class_list);
+                }
+            }
+        }
+        closedir($dh);
+
+        return $r;
+    }
+
+    /**
+     *  定義済みビュー一覧を取得する
+     *
+     *  @access public
+     *  @return array   ビュー一覧
+     */
+    function _getViewList()
+    {
+        $im =& new Ethna_InfoManager($this->backend);
+//        $view_class_list = array_keys($im->getForwardList());
+
+        $r = array();
+
+        // テンプレート/ビュースクリプトを解析する
+        $forward_list = $im->_analyzeForwardList();
+        $view_list = $this->__analyzeViewList();
+
+        // ビュー定義エントリ一覧
+        $manifest_forward_list = $im->_getForwardList_Manifest($forward_list);
+
+        // ビュー定義省略エントリ一覧
+        $implicit_forward_list = $im->_getForwardList_Implicit($forward_list, $manifest_forward_list);
+
+        $r = array_merge($view_list, $manifest_forward_list, $implicit_forward_list);
+        ksort($r);
+
+        return $r;
+    }
+
+    /**
      *  アクションテストクラスを取得する
      *
      *  @access private
@@ -48,19 +220,10 @@ class Ethna_UnitTestManager extends Ethna_AppManager
      */
     function _getTestAction()
     {
-        $em =& new Ethna_InfoManager($this->backend);
-        $action_class_list = array_keys($em->getActionList());
+        $action_class_list = array_keys($this->_getActionList());
 
         // テストの存在するアクション
-        $action_dir = $this->ctl->getActiondir();
         foreach ($action_class_list as $key => $action_name) {
-            
-            $action_path = $this->ctl->getDefaultActionPath($action_name, false);
-            if (!file_exists("$action_dir$action_path")) {
-                unset($action_class_list[$key]);
-                continue;
-            }
-            include_once $action_dir . $action_path;
             $action_class = $this->ctl->getDefaultActionClass($action_name, false).'_TestCase';
             if (!class_exists($action_class)) {
                 unset($action_class_list[$key]);
@@ -78,19 +241,10 @@ class Ethna_UnitTestManager extends Ethna_AppManager
      */
     function _getTestView()
     {
-        $em =& new Ethna_InfoManager($this->backend);
-        $view_class_list = array_keys($em->getForwardList());
+        $view_class_list = array_keys($this->_getViewList());
 
         // テストの存在するビュー
-        $view_dir = $this->ctl->getViewdir();
         foreach ($view_class_list as $key => $view_name) {
-
-            $view_path = $this->ctl->getDefaultViewPath($view_name, false);
-            if (!file_exists("$view_dir$view_path")) {
-                unset($view_class_list[$key]);
-                continue;
-            }
-            include_once $view_dir . $view_path;
             $view_class = $this->ctl->getDefaultViewClass($view_name, false).'_TestCase';
             if (!class_exists($view_class)) {
                 unset($view_class_list[$key]);
@@ -110,7 +264,7 @@ class Ethna_UnitTestManager extends Ethna_AppManager
     {
         $action_class_list = $this->_getTestAction();
         $view_class_list = $this->_getTestView();
-        
+
         $test =& new GroupTest("Ethna UnitTest");
 
         // アクション
@@ -139,7 +293,7 @@ class Ethna_UnitTestManager extends Ethna_AppManager
 
         // ActionFormのバックアップ
         $af =& $this->ctl->getActionForm();
-        
+
         //出力したい形式にあわせて切り替える
         $reporter = new Ethna_UnitTestReporter();
         $test->run($reporter);
