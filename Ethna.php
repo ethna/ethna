620099219fa6eb611984b587fe35bf6dd139d8aa
mumumu-org <mumumu-org@2ef88817-412d-0410-a32c-8029a115e976>
2007-07-15 08:58:08 +0000

    - Changed version number for 2.3.2 code FREEZE! WOW!

--- Ethna.php
+++ Ethna.php
@@ -32,7 +32,7 @@ if (!defined('DIRECTORY_SEPARATOR')) {
 }
 
 /** バージョン定義 */
-define('ETHNA_VERSION', '2.3.1');
+define('ETHNA_VERSION', '2.3.2');
 
 /** Ethnaベースディレクトリ定義 */
 define('ETHNA_BASE', dirname(__FILE__));
