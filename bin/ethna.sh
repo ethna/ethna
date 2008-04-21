6ee4e912825dd2a64c6c5e990cb43a2d1328c934
mumumu-org <mumumu-org@2ef88817-412d-0410-a32c-8029a115e976>
2008-04-21 08:23:20 +0000

    - fixed ethna command because "ethna pear-local install ethna/ethna" command cause original ethna command's misbehavior   in some condition. -- for unix only fix. -- the condition is as follows, that is , [appid]/(app|lib) directory is preceded by php default include path.
    in [appid]_Controller.php:
    
    ini_set('include_path',
            implode(PATH_SEPARATOR, array($app, $lib)) . PATH_SEPARATOR
          . ini_get('include_path')
    );

--- bin/ethna.sh
+++ bin/ethna.sh
@@ -7,15 +7,35 @@
 #   $Id$
 #
 
+CUR_DIR="$PWD"
+
 if test -z "$ETHNA_HOME"
 then
-    if test "@PEAR-DIR@" = '@'PEAR-DIR'@'
-    then
-        ETHNA_HOME="/usr/share/php/Ethna"
-    else
-        ETHNA_HOME="@PEAR-DIR@/Ethna"
-    fi
+    while [ 1 ];
+    do
+        if test -f ".ethna"
+        then
+            if test -d "$PWD""/lib/Ethna"
+            then
+                ETHNA_HOME="$PWD""/lib/Ethna"
+                break
+            fi
+        fi
+        if [ "$PWD" = "/" ];
+        then
+            if test "@PEAR-DIR@/pear" = '@'PEAR-DIR'@'
+            then
+                ETHNA_HOME="/usr/share/php/Ethna"
+            else
+                ETHNA_HOME="@PEAR-DIR@/Ethna"
+            fi
+            break
+        fi
+        cd ..
+    done
 fi
+
+cd $CUR_DIR
 
 if test -z "$PHP_COMMAND"
 then
