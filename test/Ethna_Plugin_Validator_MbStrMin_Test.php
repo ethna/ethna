1d86c280ff6eebcd53cb29ba9f6aaa236211880e
mumumu-org <mumumu-org@2ef88817-412d-0410-a32c-8029a115e976>
2008-06-07 22:30:55 +0000

    - rearranged String [Max|Min] Plugin and Tests.

---
+++ test/Ethna_Plugin_Validator_MbStrMin_Test.php
@@ -0,0 +1,46 @@
+<?php
+// vim: foldmethod=marker
+/**
+ *  Ethna_Plugin_Validator_MbStrMin_Test.php
+ */
+
+/**
+ *  Ethna_Plugin_Validator_MbStrMinクラスのテストケース
+ *
+ *  @access public
+ */
+class Ethna_Plugin_Validator_MbStrMin_Test extends Ethna_UnitTestBase
+{
+    var $vld;
+
+    function setUp()
+    {
+        $ctl =& Ethna_Controller::getInstance();
+        $plugin =& $ctl->getPlugin();
+        $this->vld = $plugin->getPlugin('Validator', 'MbStrMin');
+    }
+
+    // {{{ test min mbstr 
+    function test_min_mbstr()
+    {
+        $form_mbstr = array(
+                          'type'          => VAR_TYPE_STRING,
+                          'required'      => true,
+                          'mbstrmin'      => '3',
+                          );
+        $this->vld->af->setDef('namae_mbstr', $form_mbstr);
+
+        $pear_error = $this->vld->validate('namae_mbstr', 'あいう', $form_mbstr);
+        $this->assertFalse(is_a($pear_error, 'PEAR_Error'));
+
+        $pear_error = $this->vld->validate('namae_mbstr', 'あい', $form_mbstr);
+        $this->assertTrue(is_a($pear_error, 'PEAR_Error'));
+        $this->assertEqual(E_FORM_MIN_STRING,$pear_error->getCode());
+
+        //  TODO: Error Message Test.
+    } 
+    // }}}
+
+}
+
+?>
