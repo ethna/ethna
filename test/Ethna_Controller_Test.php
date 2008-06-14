50ad325a140832c7d9969127a4199764c4079df7
mumumu-org <mumumu-org@2ef88817-412d-0410-a32c-8029a115e976>
2008-06-14 00:52:48 +0000

    - fixed bug number was allowd as application id's first string.

--- test/Ethna_Controller_Test.php
+++ test/Ethna_Controller_Test.php
@@ -27,6 +27,59 @@ class Ethna_Controller_Test extends Ethna_UnitTestBase
         unset($GLOBALS['_Ethna_controller']);
     }
 
+    // {{{ checkAppId
+    function test_checkAppId()
+    {
+        //  予約語(app, ethna)は当然駄目
+        //  これについては大文字、小文字を区別しない
+        $r = $this->test_ctl->checkAppId('ethna');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('EthNa');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('ETHNA');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('app');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('ApP');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('APP');
+        $this->assertTrue(Ethna::isError($r));
+
+        //  数字で始まっては駄目
+        $r = $this->test_ctl->checkAppId('1');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('0abcd');
+        $this->assertTrue(Ethna::isError($r));
+
+        //  始めがアンダースコアも駄目
+        $r = $this->test_ctl->checkAppId('_');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('_abcd');
+        $this->assertTrue(Ethna::isError($r));
+
+        //  一文字でも英数字以外が混じれば駄目
+        $r = $this->test_ctl->checkAppId('ab;@e');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('@bcde');
+        $this->assertTrue(Ethna::isError($r));
+
+        $r = $this->test_ctl->checkAppId('abcd:');
+        $this->assertTrue(Ethna::isError($r));
+
+        //  全部英数字であればOK
+        $r = $this->test_ctl->checkAppId('abcd');
+        $this->assertFalse(Ethna::isError($r));
+    }
+    // }}}
+
     // {{{ test_getClientEncoding
     function test_getClientEncoding()
     {
