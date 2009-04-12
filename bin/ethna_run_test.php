c9d964f8be11691da59a8eb817b4dcebe7b94fc3
sotarok <sotarok@2ef88817-412d-0410-a32c-8029a115e976>
2009-04-12 14:26:55 +0000

    * merged: Ideas_Plugin_Extlib/bin -r710:HEAD into trunk

--- bin/ethna_run_test.php
+++ bin/ethna_run_test.php
@@ -17,7 +17,8 @@ define('ETHNA_INSTALL_BASE', dirname(dirname(__FILE__)));
 $test_dir = ETHNA_INSTALL_BASE . '/test';
 
 /** include_pathの設定(このtest runnerがあるディレクトリを追加) */
-ini_set('include_path', ETHNA_INSTALL_BASE . '/class' . PATH_SEPARATOR . ini_get('include_path'));
+ini_set('include_path', realpath(ETHNA_INSTALL_BASE . '/../') . PATH_SEPARATOR . ini_get('include_path'));
+
 
 /** Ethna関連クラスのインクルード */
 require_once ETHNA_INSTALL_BASE . '/Ethna.php';
