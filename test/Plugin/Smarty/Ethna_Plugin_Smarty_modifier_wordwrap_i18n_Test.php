8f87ac482e1c7c96bcd6647afb8b693871a05e80
maru_cc <maru_cc@2ef88817-412d-0410-a32c-8029a115e976>
2009-05-19 12:43:25 +0000

    - fixd bug smarty_modifier_wordwrap_i18n http://sourceforge.jp/ticket/browse.php?group_id=1343&tid=16839

--- test/Plugin/Smarty/Ethna_Plugin_Smarty_modifier_wordwrap_i18n_Test.php
+++ test/Plugin/Smarty/Ethna_Plugin_Smarty_modifier_wordwrap_i18n_Test.php
@@ -34,30 +34,24 @@ class Ethna_Plugin_Smarty_modifier_wordwrap_i18n_Test extends Ethna_UnitTestBase
                   . "えaおaか\n"
                   . "きaaaく\n"
                   . 'けこ';
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8);
 
-        $ctl =& new Ethna_Controller();
-        $actual = smarty_modifier_wordwrap_i18n($input_str, 8);
-        $this->assertEqual($expected, $actual);
-        unset($GLOBALS['_Ethna_controller']);
+        //    UTF-8
+        $input_str = 'あいうaえaおaかきaaaく';
+        $expected = "あいうa\n"
+                  . "えaおaか\n"
+                  . "きaaaく";
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8);
 
-        //     SJIS
-        $ctl =& new Ethna_Controller();
-        $ctl->setClientEncoding('SJIS');
-
-        $sjis_input = mb_convert_encoding($input_str, 'SJIS', 'UTF-8');
-        $sjis_expected = mb_convert_encoding($expected, 'SJIS', 'UTF-8');
-        $sjis_actual = smarty_modifier_wordwrap_i18n($sjis_input, 8);
-        $this->assertEqual($sjis_expected, $sjis_actual);
-        unset($GLOBALS['_Ethna_controller']);
-
-        //     EUC-JP
-        $ctl =& new Ethna_Controller();
-        $ctl->setClientEncoding('EUC-JP');
-
-        $eucjp_input = mb_convert_encoding($input_str, 'EUC-JP', 'UTF-8');
-        $eucjp_expected = mb_convert_encoding($expected, 'EUC-JP', 'UTF-8');
-        $eucjp_actual = smarty_modifier_wordwrap_i18n($eucjp_input, 8);
-        $this->assertEqual($eucjp_expected, $eucjp_actual);
+        //    UTF-8
+        $input_str = 'あいうaえaaaaaaaaaaaaaaaaaaaおかahaかきaaaく';
+        $expected = "あいうa\n"
+                  . "えaaaaaa\n"
+                  . "aaaaaaaa\n"
+                  . "aaaaaお\n"
+                  . "かahaか\n"
+                  . "きaaaく";
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8);
     }
 
     function test_smarty_modifier_wordwrap_i18n_indent()
@@ -67,53 +61,77 @@ class Ethna_Plugin_Smarty_modifier_wordwrap_i18n_Test extends Ethna_UnitTestBase
         //    インデントされないので注意
         //
 
-        unset($GLOBALS['_Ethna_controller']);
-
         //    UTF-8
         $input_str = 'あいうaえaおaかきaaaくけこ';
         $expected = "あいうa\n"
                   . "    えaおaか\n"
                   . "    きaaaく\n"
                   . '    けこ';
-
-        $ctl =& new Ethna_Controller();
-        $actual = smarty_modifier_wordwrap_i18n($input_str, 8, "\n", 4);
-        $this->assertEqual($expected, $actual);
-        unset($GLOBALS['_Ethna_controller']);
-
-        //     SJIS
-        $ctl =& new Ethna_Controller();
-        $ctl->setClientEncoding('SJIS');
-
-        $sjis_input = mb_convert_encoding($input_str, 'SJIS', 'UTF-8');
-        $sjis_expected = mb_convert_encoding($expected, 'SJIS', 'UTF-8');
-        $sjis_actual = smarty_modifier_wordwrap_i18n($sjis_input, 8, "\n", 4);
-        $this->assertEqual($sjis_expected, $sjis_actual);
-        unset($GLOBALS['_Ethna_controller']);
-
-        //     EUC-JP
-        $ctl =& new Ethna_Controller();
-        $ctl->setClientEncoding('EUC-JP');
-
-        $eucjp_input = mb_convert_encoding($input_str, 'EUC-JP', 'UTF-8');
-        $eucjp_expected = mb_convert_encoding($expected, 'EUC-JP', 'UTF-8');
-        $eucjp_actual = smarty_modifier_wordwrap_i18n($eucjp_input, 8, "\n", 4);
-        $this->assertEqual($eucjp_expected, $eucjp_actual);
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8, "\n", 4);
     }
 
     function test_smarty_modifier_wordwrap_i18n_break()
     {
-        unset($GLOBALS['_Ethna_controller']);
-
         //    UTF-8
         $input_str = 'あいうaえaおaかきaaaくけこ';
         $expected = "あいうa\r\n"
                   . "    えaおaか\r\n"
                   . "    きaaaく\r\n"
                   . '    けこ';
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8, "\r\n", 4);
+    }
+
+    function test_smarty_modifier_wordwrap_i18n_space()
+    {
+        //    UTF-8
+        $input_str = 'あいうaえaおaかきaaaくけこ     ';
+        $expected = "あいうa\r\n"
+                  . "    えaおaか\r\n"
+                  . "    きaaaく\r\n"
+                  . "    けこ    \r\n"
+                  . '     ';
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8, "\r\n", 4);
+    }
+
+    function test_smarty_modifier_wordwrap_i18n_kana()
+    {
+        //    UTF-8
+        $input_str = 'あいうｲｴｵaえaおaかきaaaくけこ';
+        $expected = "あいうｲｴ\r\n"
+                  . "    ｵaえaおa\r\n"
+                  . "    かきaaa\r\n"
+                  . '    くけこ';
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8, "\r\n", 4);
+    }
+
+    function test_smarty_modifier_wordwrap_i18n_alphabet()
+    {
+        $input_str = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+        $expected = 'abcdefgh<br />'
+                  . 'ijklmnop<br />'
+                  . 'qrstuvwx<br />'
+                  . 'yzabcdef<br />'
+                  . 'ghijklmn<br />'
+                  . 'opqrstuv<br />'
+                  . 'wxyz';
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8, '<br />');
+
+        $input_str = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuv';
+        $expected = 'abcdefgh<br />'
+                  . 'ijklmnop<br />'
+                  . 'qrstuvwx<br />'
+                  . 'yzabcdef<br />'
+                  . 'ghijklmn<br />'
+                  . 'opqrstuv';
+        $this->_test_smarty_modifier_wordwrap_i18n($expected, $input_str, 8, '<br />');
+    }
+
+    function _test_smarty_modifier_wordwrap_i18n($expected, $input_str, $width, $break = "\n", $indent = 0)
+    {
+        unset($GLOBALS['_Ethna_controller']);
 
         $ctl =& new Ethna_Controller();
-        $actual = smarty_modifier_wordwrap_i18n($input_str, 8, "\r\n", 4);
+        $actual = smarty_modifier_wordwrap_i18n($input_str, $width, $break, $indent);
         $this->assertEqual($expected, $actual);
         unset($GLOBALS['_Ethna_controller']);
 
@@ -123,7 +141,7 @@ class Ethna_Plugin_Smarty_modifier_wordwrap_i18n_Test extends Ethna_UnitTestBase
 
         $sjis_input = mb_convert_encoding($input_str, 'SJIS', 'UTF-8');
         $sjis_expected = mb_convert_encoding($expected, 'SJIS', 'UTF-8');
-        $sjis_actual = smarty_modifier_wordwrap_i18n($sjis_input, 8, "\r\n", 4);
+        $sjis_actual = smarty_modifier_wordwrap_i18n($sjis_input, $width, $break, $indent);
         $this->assertEqual($sjis_expected, $sjis_actual);
         unset($GLOBALS['_Ethna_controller']);
 
@@ -133,7 +151,7 @@ class Ethna_Plugin_Smarty_modifier_wordwrap_i18n_Test extends Ethna_UnitTestBase
 
         $eucjp_input = mb_convert_encoding($input_str, 'EUC-JP', 'UTF-8');
         $eucjp_expected = mb_convert_encoding($expected, 'EUC-JP', 'UTF-8');
-        $eucjp_actual = smarty_modifier_wordwrap_i18n($eucjp_input, 8, "\r\n", 4);
+        $eucjp_actual = smarty_modifier_wordwrap_i18n($eucjp_input, $width, $break, $indent);
         $this->assertEqual($eucjp_expected, $eucjp_actual);
     }
     // }}}
