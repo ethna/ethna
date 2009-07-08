c353e7408a1a331d0df0542b08d41db631b8adac
Sotaro KARASAWA <sotaro.k@gmail.com>
2009-07-08 13:41:45 +0900

    Add Ethna_Plugin_Abstract for all plugins abstract

--- class/Plugin/Abstract.php
+++ class/Plugin/Abstract.php
@@ -23,13 +23,20 @@ class Ethna_Plugin_Abstract
      *  @access private
      */
 
-    var $controller;
+    var $type = null;
+
+    var $name = null;
+
 
     /** @var    object  Ethna_Controller    Controller Object */
+    var $controller;
+    /** @var    object  Ethna_Controller    Alias */
     var $ctl;
 
-    /** @var    object  Ethna_Config        Configure Object */
+    /** @var    array   plugin configure */
     var $config;
+
+    var $config_default = array();
 
     /** @var    object  Ethna_Logger        Logger Object */
     var $logger;
@@ -41,13 +48,109 @@ class Ethna_Plugin_Abstract
      *  @param  object  Ethna_Controller    &$controller    Controller Object
      */
     // function __construct(&$controller)
-    function Ethna_Plugin_Abstract(&$controller)
+    function Ethna_Plugin_Abstract(&$controller, $type = null, $name = null)
     {
         $this->controller =& $controller;
         $this->ctl =& $this->controller;
 
-        $this->config =& $controller->getConfig();
+        //$this->config =& $controller->getConfig();
         $this->logger =& $this->controller->getLogger();
+
+        //
+        $this->type = $type;
+        $this->name = $name;
+
+        // if constractor called without parameter $type or $name, auto detect type and name of self.
+        if ($this->type === null) {
+            $this->type = $this->_detectType();
+        }
+
+        if ($this->name === null) {
+            $this->name = $this->_detectName();
+        }
+
+        // load plugin hook
+        $this->_onLoad();
     }
 
+    /**
+     *  getType
+     *
+     *  @access public
+     */
+    function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     *  getType
+     *
+     *  @access public
+     */
+    function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     *  getType
+     *
+     *  @access protected
+     */
+    function _onLoad()
+    {
+        $this->_loadConfig();
+    }
+
+    /**
+     *  getType
+     *
+     *  @access protected
+     */
+    function _loadConfig()
+    {
+        $config =& $this->ctl->getConfig();
+        $plugin_config = $config->get('plugin');
+
+        if ($plugin_config === null || !isset($plugin_config[$this->type])) {
+            $this->config = $this->config_default;
+        }
+        else {
+            $this->config = array_merge($this->config_default, $plugin_config[$this->type]);
+        }
+        return true;
+    }
+
+    /**
+     *  getType
+     *
+     *  @access protected
+     */
+    function _detectType()
+    {
+        $type = array_shift(explode("_", str_replace("Ethna_Plugin_", get_class($this))));
+        if ($type !== "") {
+            return $type;
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     *  getType
+     *
+     *  @access protected
+     */
+    function _detectName()
+    {
+        $name = explode("_", str_replace("Ethna_Plugin_", "", get_class($this)));
+        if (count($name) === 2) {
+            return $name[1];
+        }
+        else {
+            return null;
+        }
+    }
 }
---
+++ test/Plugin/Abstract/Ethna_Plugin_Abstract_Test.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  Ethna_Plugin_Abstract_Test.php
+ */
+
+/**
+ *  Ethna_Plugin_Abstract クラスのテストケース
+ *
+ *  @access public
+ */
+class Ethna_Plugin_Abstract_Test extends Ethna_UnitTestBase
+{
+    var $plugin;
+    var $lw;
+    var $abstract;
+
+    function setUp()
+    {
+        $this->plugin =& $this->ctl->getPlugin();
+
+        // for PHP 5, it's not enable to create instance of abstract class,
+        // now this is temporary process.
+        $this->abstract = $this->plugin->getPlugin('Abstract', null);
+
+        $this->lw = $this->plugin->getPlugin('Logwriter', 'Echo');
+
+    }
+
+    function testDetectTypeAndName()
+    {
+        $this->assertEqual('Abstract', $this->abstract->getType());
+        $this->assertEqual(null, $this->abstract->getName());
+
+        $this->assertEqual('Logwriter', $this->lw->getType());
+        $this->assertEqual('Echo', $this->lw->getName());
+
+        var_dump($this->abstract->config);
+    }
+}
+?>
