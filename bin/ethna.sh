c4cb2d4f182ae49c58515b0d29daa478d4d60b5c
Sotaro Karasawa <reg@sotaro-k.com>
2009-07-19 17:51:43 +0900

    extlib以下にプラグインを先にインストールすると、ethnaコマンドが使えなくなる問題を修正
     - ethna コマンドは、 lib 以下に Ethna ディレクトリが存在する場合は lib 以下のEthnaを使う仕様になっていたが、
       この場合 exlib 以下にプラグインをインストールすると Ethna が設置されずにEthnaディレクトリが存在する可能性がある
     - Ethna/Ethna.php が存在するかどうかを判断するように変更

--- bin/ethna.sh
+++ bin/ethna.sh
@@ -4,7 +4,7 @@
 #
 #   simple command line gateway
 #
-#   $Id: ethna.sh 439 2007-01-04 06:23:15Z ichii386 $
+#   $Id$
 #
 
 CUR_DIR="$PWD"
@@ -15,7 +15,7 @@ then
     do
         if test -f ".ethna"
         then
-            if test -d "$PWD""/lib/Ethna"
+            if test -f "$PWD""/lib/Ethna/Ethna.php"
             then
                 ETHNA_HOME="$PWD""/lib/Ethna"
                 break
