eb6a6a512b1685b4255721007d925c0423145457
okonomi <oknm@okonomi.sakura.ne.jp>
2011-10-17 10:03:28 +0900

    fixed: some directory settings.

--- bin/ethna_run_test.php
+++ bin/ethna_run_test.php
@@ -37,6 +37,7 @@ if (basename(ETHNA_INSTALL_BASE) != 'Ethna') {
 
 /** テストケースがあるディレクトリ */
 $test_dir = ETHNA_INSTALL_BASE . '/test';
+$test_lib_dir = ETHNA_INSTALL_BASE . '/test/lib';
 
 /** include_pathの設定(このtest runnerがあるディレクトリを追加) */
 //ini_set('include_path', realpath(ETHNA_INSTALL_BASE . '/class') . PATH_SEPARATOR . ini_get('include_path'));
@@ -54,9 +55,9 @@ if (extension_loaded('xdebug')) {
 /** SimpleTestのインクルード */
 require_once 'simpletest/unit_tester.php';
 require_once 'simpletest/reporter.php';
-require_once $test_dir . '/TextSimpleReporter.php';
-require_once $test_dir . '/TextDetailReporter.php';
-require_once $test_dir . '/UnitTestBase.php';
+require_once $test_lib_dir . '/TextSimpleReporter.php';
+require_once $test_lib_dir . '/TextDetailReporter.php';
+require_once $test_lib_dir . '/UnitTestBase.php';
 
 $test = new TestSuite('Ethna All tests');
 
