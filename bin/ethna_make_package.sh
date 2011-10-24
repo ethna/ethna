ceb10b70ea23bfd7aa85bf27a3fc5ce527664f64
Sotaro KARASAWA <sotarok@crocos.co.jp>
2011-10-25 00:42:12 +0900

    added Smarty3, simpletest1.1 to build script

--- bin/ethna_make_package.sh
+++ bin/ethna_make_package.sh
@@ -37,10 +37,12 @@ rm -f $basedir/package.xml
 
 rm -fr $targetdir
 mkdir $targetdir
-cp -pR * "$targetdir/"
+git checkout-index -a -f --prefix="$targetdir/"
 
 #  create optional package
 optpkg_dir="$targetdir/misc/optional_package"
+
+# Smarty2
 mkdir $optpkg_dir/Smarty/release
 cd $optpkg_dir/Smarty/src
 tar xvfz Smarty*.tar.gz
@@ -48,6 +50,17 @@ cd $optpkg_dir/Smarty/build
 chmod +x ./build
 ./build
 cp $optpkg_dir/Smarty/release/*.tgz $tmpdir
+
+# Smarty3
+mkdir $optpkg_dir/Smarty3/release
+cd $optpkg_dir/Smarty3/src
+tar xvfz Smarty*.tar.gz
+cd $optpkg_dir/Smarty3/build
+chmod +x ./build
+./build
+cp $optpkg_dir/Smarty3/release/*.tgz $tmpdir
+
+# simpletest 1.0
 mkdir $optpkg_dir/simpletest/release
 cd $optpkg_dir/simpletest/src
 tar xvfz simpletest*.tar.gz
@@ -55,10 +68,20 @@ cd $optpkg_dir/simpletest/build
 chmod +x ./build
 ./build
 cp $optpkg_dir/simpletest/release/*.tgz $tmpdir
+
+# simpletest 1.1
+mkdir $optpkg_dir/simpletest1.1/release
+cd $optpkg_dir/simpletest1.1/src
+tar xvfz simpletest*.tar.gz
+cd $optpkg_dir/simpletest1.1/build
+chmod +x ./build
+./build
+cp $optpkg_dir/simpletest1.1/release/*.tgz $tmpdir
+
 rm -rf $optpkg_dir
 cd $basedir
 
-find $targetdir -name "CVS" -o -name ".svn" | xargs rm -fr
+find $targetdir -name "CVS" -o -name ".svn" -o -name ".git*" | xargs rm -fr
 
 # create package for php 5
 php $basedir/bin/ethna_make_package.php $beta
