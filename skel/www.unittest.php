eb7888b54e6d6458e810a5530a09e60ab6cf54a9
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2014-02-07 00:06:34 +0900

    remove old unit test classes

--- skel/www.unittest.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-require_once dirname(__FILE__) . '/../app/{$project_id}_Controller.php';
-
-{$project_id}_Controller::main('{$project_id}_Controller', array(
-    '__ethna_unittest__',
-    )
-);
